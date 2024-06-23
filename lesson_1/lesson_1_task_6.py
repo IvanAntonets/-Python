8cf337feed6dabd294c0e04c91f06aa5364d0772
IvanAntonets <iwan.antonets37@yandex.r>
2024-06-23 15:47:27 +0300

    first hw

---
+++ lesson_1/lesson_1_task_6.py
@@ -0,0 +1,23 @@
+# "Вывести в консоль" заменил на "a", для упрощения ввода 
+def a1():
+    print("1", end="")
+def a2():
+    print("2", end="")
+def a3():
+    print("3", end="")
+def a4():
+    print("4", end="")
+def a5():
+    print("5", end="")
+def a6():
+    print("6", end="")
+def a7():
+    print("7", end="")
+def a8():
+    print("8", end="")
+def a9():
+    print("9", end="")
+def a0():
+    print("0", end="")
+
+a8(),a8(),a0(),a0(),a5(),a5(),a5(),a3(),a5(),a3(),a5()
---
+++ lesson_1/lesson_1_task_7.py
@@ -0,0 +1,4 @@
+def chi(num):
+    print(num, end="")
+
+chi("8"), chi("8"),chi("0"),chi("0"),chi("5"),chi("5"),chi("5"),chi("3"),chi("5"),chi("3"),chi("5")
